599d9ced8c7a1e775b4dc8bdf1cacf3b24f429bb
Kandrexz24 <clau.281andrex@gmail.com>
2020-05-28 21:39:19 -0400

    header

---
+++ header.php
@@ -0,0 +1,25 @@
+ <!-- COMIENZO HEADER -->
+ <nav class="navbar navbar-expand-lg  navbar-dark bg-dark sticky-top">
+    <a class="navbar-brand" href="index.html"><img src="png/icono-paginas-web.png" style="height:40px"><img></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav m-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Principal <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacto</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Iniciar Sesion</a>
+        </li>
+      </ul>
+      <span class="navbar-text">
+      <a class="nav-link" href="login.php">Iniciar Sesion</a>
+      </span>
+    </div>
+  </nav>
+  <!-- TERMINO HEADER -->
