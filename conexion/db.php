924057c23905e5e3521d9c741e07d10c45d4d1ce
Kandrexz24 <clau.281andrex@gmail.com>
2020-06-13 14:55:40 -0400

    coomit db

--- conexion/db.php
+++ conexion/db.php
@@ -1,4 +1,23 @@
 <?php
+//https://codea.app/php/conexion-y-metodos-de-consulta-a-una-bd-mysql
+echo "<label>";
+class ApptivaDB{    
+
+    private $ServerName = "181.72.63.189";
+    private $NameBD = "ccmdata";
+    private $Username = "userccmshop";
+    private $Password = "ccmshop";
+    public $conexion;
+    public function __construct(){
+        $this->conexion = new mysqli($this->ServerName, $this->Username, $this->Password,$this->NameBD) or die(mysql_error());
+        $this->conexion->set_charset("utf8");
+    };
+
+  };
+
+
+/*
+
 // Declaramos las variables de conexión
 $ServerName = "181.72.63.189";
 $NameBD = "ccmdata";
@@ -6,9 +25,17 @@ $Username = "userccmshop";
 $Password = "ccmshop";
 
 try {
-    $conn = new PDO("mysql:host=$ServerName;dbname=$NameBD;", $Username, $Password);
+    // $conn = new PDO("mysql:host=$ServerName;dbname=$NameBD;", $Username, $Password);
+  $link = mysqli_connect($ServerName, $Username,$Password, $NameBD);
     echo "Conectado correctamente";
   } catch (PDOException $e) {
     die('Connection Failed: ' . $e->getMessage());
   }
+*/
+
+
+
 ?> 
+
+
+
