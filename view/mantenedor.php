a72512f338f9ec74cbdf3127804c1cbb6b142405
Kandrexz24 <clau.281andrex@gmail.com>
2020-06-20 02:12:07 -0400

    commmit mvcc practico 2

---
+++ view/mantenedor.php
@@ -0,0 +1,158 @@
+<!-- <?php "conexion/db.php" ?> -->
+<!-- <?php include "includes/header_admin_mant.php" ?> -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head> 
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin 2 - Dashboard</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap-grid.min.css" rel="stylesheet">
+  <link href="css/bootstrap-reboot.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/admin.css" rel="stylesheet">
+
+</head>
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+
+  <?php include "Includes/header_nav.php" ?>
+
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+  <!-- Main Content -->
+  <div id="content">
+
+  <!-- Topbar Navbar -->
+
+<?php include "Includes/header_hor_admin.php" ?>
+<!-- End of Topbar -->
+        <!-- contenido de la pagina -->
+        <div class="container-fluid">
+
+          <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+              <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Usuario</a>
+            </li>
+            <li class="nav-item" role="presentation">
+              <a class="nav-link" id="tab_estado_tab" data-toggle="pill" href="#tab_estado" role="tab" aria-controls="pills-contact" aria-selected="false">Estado</a>
+            </li>
+          </ul>
+
+          <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+              <form action="cruds/save_region2.php" method="POST">
+                <div class="form-group">
+                  <label for="formGroupExampleInput">Codigo region</label>
+                  <input type="text" name="cod_region" class="form-control" placeholder="Inserte codigo">
+                </div>
+                <div class="form-group">
+                  <label for="formGroupExampleInput2">Region</label>
+                  <input type="text" name="desc_region" class="form-control" placeholder="Inserte region">
+                </div>
+                <div class="form-group">
+                  <input type="submit" class="btn btn-primary" name="save_reg" value="Submit">
+                </div>
+              </form>
+            </div>
+           
+            <div class="tab-pane fade" id="tab_estado" role="tabpanel" >
+              <form>
+                <div class="form-group">
+                  <label for="formGroupExampleInput">Example label</label>
+                  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+                </div>
+                <div class="form-group">
+                  <label for="formGroupExampleInput2">Another label</label>
+                  <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                </div>
+                <div class="form-group">
+                  <input class="btn btn-primary" type="submit" value="Submit">
+                </div>
+              </form>
+            </div>
+          </div>
+
+          
+        
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.php">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div>
+   <!-- Bootstrap core JavaScript-->
+   <script src="vendor/jquery/jquery.min.js"></script>
+   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+ 
+   <!-- Core plugin JavaScript-->
+   <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+ 
+   <!-- Custom scripts for all pages-->
+   <script src="js/admin.js"></script>
+ 
+   <!-- Page level plugins -->
+   <script src="vendor/chart.js/Chart.min.js"></script>
+ 
+   <!-- Page level custom scripts -->
+   <script src="js/demo/chart-area-demo.js"></script>
+   <script src="js/demo/chart-pie-demo.js"></script>
+   <script src="js/demo/chart-bar-demo.js"></script>
+
+</body>
+
+</html>
